8f31a16c0c410ec53eee80d3908fb83d8558d111
beb777 <bebatbeb@gmail.com>
2021-09-26 15:37:27 +0300

    adding song

---
+++ py.py
@@ -0,0 +1,30 @@
+
+# song -y [ song_name ]
+# song -d [ song_name | singer_name | movie_name ]
+# song -d --download-all [ singer_name | movie_name ]
+# song --ty [ textfile.txt ]
+# song --td [ textfile.txt ]
+# song --version
+#
+# Optional arguments are:
+#
+#     -y : For downloading mp3 from youtube
+#     -d : For downloading mp3 from mr-jatt
+#     --ty : For downloading all songs in specified text file from youtube mp3 format
+#     --td : For downloading all songs in specified text file from mr-jatt mp3 format
+#     --download-all : For downloading all songs of a movie or top songs of a singer
+#     --version : For printing song version
+# For Installing
+#
+# pip install song
+#
+# For Upgrading
+#
+# pip install --upgrade song
+#
+# For Uninstalling
+#
+# pip uninstall song
+
+https://www.youtube.com/watch?v=hzqFmXZ8tOE&list=PLp7pAH9am84O9Cd9VlW0W_qVVCL5PC3BI
+song -d sunidhi chauhan
