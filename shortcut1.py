f9156abc6c170212d52b244a0c89954a010d8e0e
beb777 <bebatbeb@gmail.com>
2021-09-27 15:36:03 +0300

    number separation to make more readable using underscore

--- shortcut1.py
+++ shortcut1.py
@@ -1,3 +1,5 @@
+
+
 condition = True
 if condition:
     x = 1
@@ -11,3 +13,16 @@ shortcut for the above statement
 condition = False
 x = 1 if condition else 0
 print(x)
+'''
+number separation to make more readable using underscore 
+'''
+z = 1000000000000
+y = 200000000000
+sum = z + y
+print(sum)
+
+z = 1_000_000_000_000
+y = 200_000_000_000
+sum = z + y
+print(sum)
+print(f'{sum:,}')
